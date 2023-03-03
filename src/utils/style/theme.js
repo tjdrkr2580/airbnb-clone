e92276e7cacfa9ebaf894360c3c857bb5d56d341
dajeong <daaa_j@naver.com>
2023-03-03 15:03:09 +0900

    feat: theme

--- src/utils/style/theme.js
+++ src/utils/style/theme.js
@@ -1,1 +1,7 @@
-export const theme = {};
+export const theme = {
+    mainColor: '#ff385c',
+    unselectColor: '#717171',
+    selectColor1: '#5d5d5d',
+    selectColor2: '#707070',
+    boldColor: '#222222',
+};
