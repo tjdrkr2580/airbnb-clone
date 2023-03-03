9af9696017dbf128f0decb3b0ef2506936443d69
tjdrkr2580 <tjdrkr2580@naver.com>
2023-03-03 12:24:56 +0900

    setting : 세팅

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { RecoilRoot } from "recoil";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
@@ -16,9 +17,11 @@ const queryClient = new QueryClient({
 
 root.render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </RecoilRoot>
   </React.StrictMode>
 );
 reportWebVitals();
--- src/utils/style/theme.js
+++ src/utils/style/theme.js
@@ -1,2 +1,1 @@
-export const lightTheme = {};
-export const darkTheme = {};
+export const theme = {};
