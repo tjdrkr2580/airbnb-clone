9e997b5bea32e921cda9b8b9b3d7d18532647ad1
tjdrkr2580 <tjdrkr2580@naver.com>
2023-03-03 16:37:20 +0900

    feat : mixin으로 export 후 Home 컴포넌트 정리

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,47 +1,7 @@
-import HotelElement from "element/HotelElement";
 import React from "react";
-import styled from "styled-components";
-import { HotelGridLayoutStyle } from "utils/style/mixins";
-
-const HomeWrapper = styled.section`
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
-`;
-
-const HotelGridWrapper = styled.ul`
-  ${HotelGridLayoutStyle}
-`;
 
 const Home = () => {
-  return (
-    <HomeWrapper>
-      <HotelGridWrapper>
-        <HotelElement />
-        <HotelElement />
-        <HotelElement />
-        <HotelElement />
-        <HotelElement />
-        <HotelElement />
-        <HotelElement />
-        <HotelElement />
-        <HotelElement />
-        <HotelElement />
-        <HotelElement />
-        <HotelElement />
-        <HotelElement />
-        <HotelElement />
-        <HotelElement />
-        <HotelElement />
-        <HotelElement />
-        <HotelElement />
-        <HotelElement />
-        <HotelElement />
-        <HotelElement />
-        <HotelElement />
-      </HotelGridWrapper>
-    </HomeWrapper>
-  );
+  return <div className="App"></div>;
 };
 
 export default Home;
