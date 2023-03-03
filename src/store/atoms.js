0887eac58c1918135ed2d46f465ae3bc9a94180c
tjdrkr2580 <tjdrkr2580@naver.com>
2023-03-03 20:17:25 +0900

    feat : atom login modal

--- src/store/atoms.js
+++ src/store/atoms.js
@@ -1,1 +1,12 @@
 import { atom } from "recoil";
+import { v4 } from "uuid";
+
+export const isLoginModal = atom({
+  key: `state${v4()}`,
+  default: false,
+});
+
+// export const isLogin = atom({
+//     key : `state${v4()}`,
+//     default : false,
+// })
