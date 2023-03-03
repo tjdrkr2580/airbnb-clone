921a0afbfe7becc19651e2d740507e09699f7521
dajeong <daaa_j@naver.com>
2023-03-03 23:15:30 +0900

    feat: footer layout

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import styled from 'styled-components';
-import { HeaderFooterStyle, flexRowCenter, flexColumnCenter, boxBorderRadius } from 'utils/style/mixins';
+import { HeaderFooterStyle } from 'utils/style/mixins';
 
 const Footer = () => {
-    return <FooterContainer>Footer</FooterContainer>;
+    return (
+        <FooterContainer>
+            <span>© 2023 hanghae cloncoding 3조, Inc.</span>
+            <span>|</span>
+            <span>개인정보 처리방침</span>
+            <span>|</span>
+            <span>이용약관</span>
+            <span>|</span>
+            <span>사이트맵</span>
+        </FooterContainer>
+    );
 };
 
 export default Footer;
 
 const FooterContainer = styled.div`
     ${HeaderFooterStyle}
+    justify-content: center;
+    gap: 20px;
+    border-top: 2px solid ${(props) => props.theme.borderColor};
+    span {
+        font-size: 1.2rem;
+    }
 `;
