7278ba908a09cde14f87c9976ddae95cffddb8c7
dajeong <daaa_j@naver.com>
2023-03-04 11:20:13 +0900

    feat: button style

--- src/element/Button.jsx
+++ src/element/Button.jsx
@@ -1,20 +1,25 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
+import { boxBorderRadius } from '../utils/style/mixins';
 
-const CustomBtn = styled.button``;
+const CustomBtn = styled.button`
+    width: ${(props) => (props.isType ? '85%' : '150px')};
+    color: ${(props) => (props.isColor ? 'white' : 'black')};
+    background: ${(props) => props.isBackground && props.theme.mainColor};
+    border: ${(props) => (props.isBorder ? `1px solid ${props.theme.borderColor}` : 'none')};
+    ${boxBorderRadius};
+`;
 
 const Button = ({ children, ...props }) => {
-  return (
-    <CustomBtn
-      isType={props.type}
-      isBackground={props.isBackground}
-      isBorder={props.isBorder}
-      isColor={props.isColor}
-      onClick={props.onClick}
-    >
-      {children}
-    </CustomBtn>
-  );
+    return (
+        <CustomBtn isType={props.type} isBackground={props.isBackground} isBorder={props.isBorder} isColor={props.isColor} onClick={props.onClick}>
+            {children}
+        </CustomBtn>
+    );
 };
 
 export default Button;
+
+Button.defaultProps = {
+    onClick: () => {},
+};
