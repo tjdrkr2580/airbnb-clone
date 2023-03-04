15c2f182548efbd2ce76caf1dbc074a547a3a219
tjdrkr2580 <tjdrkr2580@naver.com>
2023-03-04 09:14:31 +0900

    fix : layout, login modal visible

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
-import GlobalStyle from 'utils/style/GlobalStyle';
-import Header from 'components/Header';
-import Footer from 'components/Footer';
-import Home from 'pages/Home';
-import { theme } from 'utils/style/theme';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import GlobalStyle from "utils/style/GlobalStyle";
+import Header from "components/Header";
+import Footer from "components/Footer";
+import Home from "pages/Home";
+import { theme } from "utils/style/theme";
+import { useRecoilValue } from "recoil";
+import { isLoginModalState } from "store/atoms";
+import LoginModal from "components/LoginModal";
 
 function App() {
-    return (
-        <ThemeProvider theme={theme}>
-            <BrowserRouter>
-                <GlobalStyle />
-                <Header />
-                <Routes>
-                    <Route element={<Home />} path="/" />
-                </Routes>
-                <Footer />
-            </BrowserRouter>
-        </ThemeProvider>
-    );
+  const isVisibleLoginModal = useRecoilValue(isLoginModalState);
+  return (
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <GlobalStyle />
+        <Header />
+        <Routes>
+          <Route element={<Home />} path="/" />
+        </Routes>
+        <Footer />
+        {isVisibleLoginModal === true && <LoginModal />}
+      </BrowserRouter>
+    </ThemeProvider>
+  );
 }
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,154 +1,171 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import styled from 'styled-components';
-import { BiSearchAlt, BiUser } from 'react-icons/bi';
-import { AiOutlineMenu, AiOutlinePlusCircle } from 'react-icons/ai';
-import { useState } from 'react';
-import { HeaderFooterStyle, flexRowCenter, flexColumnCenter, boxBorderRadius } from 'utils/style/mixins';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import { BiSearchAlt, BiUser } from "react-icons/bi";
+import { AiOutlineMenu, AiOutlinePlusCircle } from "react-icons/ai";
+import {
+  HeaderFooterStyle,
+  flexRowCenter,
+  flexColumnCenter,
+  boxBorderRadius,
+} from "utils/style/mixins";
+import { useSetRecoilState } from "recoil";
+import { isLoginModalState } from "store/atoms";
 
 const Header = () => {
-    const navigate = useNavigate();
-    const [showModal, setShowModal] = useState(false);
+  const navigate = useNavigate();
+  const setVisibleLoginModal = useSetRecoilState(isLoginModalState);
+  const [showModal, setShowModal] = useState(false);
 
-    // 버튼 클릭시
-    const showMyInfo = () => {
-        setShowModal(!showModal);
-    };
+  const showMyInfo = () => {
+    setShowModal(!showModal);
+  };
 
-    return (
-        <HeaderContainer>
-            <LogoImg src={process.env.PUBLIC_URL + 'logo.png'} />
-            <button style={{ backgroundColor: 'white' }}>
-                <SearchContainer>
-                    <SearchText>숙소 검색</SearchText>
-                    <SearchIconBox>
-                        <BiSearchAlt size={16} />
-                    </SearchIconBox>
-                </SearchContainer>
-            </button>
-            {/* <button
-                style={{ backgroundColor: 'white' }}
-                onClick={() => {
-                    navigate('/login');
-                }}
-            >
-                <LoginButtonBox>로그인</LoginButtonBox>
-            </button> */}
-            {/* 로그인 했을때 마이페이지 버튼 */}
-            <LoginUserButtonContainer>
-                <button style={{ backgroundColor: 'white' }}>
-                    <AiOutlinePlusCircle size={22} />
-                </button>
-                <button style={{ backgroundColor: 'white' }} onClick={showMyInfo}>
-                    <LoginButtonBox>
-                        <AiOutlineMenu size={16} />
-                        <BiUser size={16} />
-                        <img></img>
-                    </LoginButtonBox>
-                </button>
-                <MyInfoModalContainer isShow={showModal}>
-                    <UserEmail>
-                        <span>jeong@naver.com</span>
-                    </UserEmail>
-                    <UserPage>
-                        <span>마이페이지</span>
-                    </UserPage>
-                    <LogoutButton>
-                        <button style={{ backgroundColor: 'transparent' }}>로그아웃</button>
-                    </LogoutButton>
-                </MyInfoModalContainer>
-            </LoginUserButtonContainer>
-        </HeaderContainer>
-    );
+  return (
+    <HeaderContainer>
+      <LogoImg
+        onClick={() => navigate("/")}
+        src={process.env.PUBLIC_URL + "logo.png"}
+      />
+      <TransparentBtn>
+        <SearchContainer>
+          <SearchText>숙소 검색</SearchText>
+          <SearchIconBox>
+            <BiSearchAlt size={16} />
+          </SearchIconBox>
+        </SearchContainer>
+      </TransparentBtn>
+      {/* <button
+        style={{ backgroundColor: "white" }}
+        onClick={() => {
+          navigate("/login");
+        }}
+      >
+        <LoginButtonBox>로그인</LoginButtonBox>
+      </button> */}
+      <LoginUserButtonContainer>
+        <TransparentBtn>
+          <AiOutlinePlusCircle size={22} />
+        </TransparentBtn>
+        <TransparentBtn>
+          <LoginButtonBox onClick={() => setVisibleLoginModal(true)}>
+            <AiOutlineMenu size={16} />
+            <BiUser size={16} />
+            <section />
+          </LoginButtonBox>
+        </TransparentBtn>
+        <MyInfoModalContainer>
+          <UserEmail>
+            <span>jeong@naver.com</span>
+          </UserEmail>
+          <UserPage>
+            <span>마이페이지</span>
+          </UserPage>
+          <LogoutButton>
+            <TransparentBtn>로그아웃</TransparentBtn>
+          </LogoutButton>
+        </MyInfoModalContainer>
+      </LoginUserButtonContainer>
+    </HeaderContainer>
+  );
 };
 
 export default Header;
 
 const HeaderContainer = styled.div`
-    ${HeaderFooterStyle}
-    border-bottom: 2px solid ${(props) => props.theme.borderColor};
+  z-index: 998;
+  background-color: white;
+  position: fixed;
+  top: 0;
+  left: 0;
+  ${HeaderFooterStyle}
+  border-bottom: 2px solid ${(props) => props.theme.borderColor};
 `;
 
 const LogoImg = styled.img`
-    width: 11rem;
+  width: 12rem;
 `;
 
 const SearchContainer = styled.div`
-    ${flexRowCenter};
-    min-width: 20rem;
-    height: 4.8rem;
-    border-radius: 6.25rem;
-    border: 1.5px solid ${(props) => props.theme.borderColor};
-    box-shadow: 0px 3px 15px 0px rgba(0, 0, 0, 0.1);
-    cursor: pointer;
-    &:hover {
-        box-shadow: 0px 3px 5px 0px rgba(0, 0, 0, 0.1);
-    }
+  ${flexRowCenter};
+  min-width: 20rem;
+  height: 4.8rem;
+  border-radius: 6.25rem;
+  border: 1.5px solid ${(props) => props.theme.borderColor};
+  box-shadow: 0px 3px 15px 0px rgba(0, 0, 0, 0.1);
+  cursor: pointer;
+  &:hover {
+    box-shadow: 0px 3px 5px 0px rgba(0, 0, 0, 0.1);
+  }
 `;
 
 const SearchText = styled.div`
-    padding: 0px 16px;
-    font-size: 1.6rem;
+  padding: 0px 16px;
+  font-size: 1.6rem;
 `;
 
 const SearchIconBox = styled.div`
-    ${flexRowCenter};
-    width: 3.2rem;
-    height: 3.2rem;
-    border-radius: 50%;
-    background-color: ${(props) => props.theme.mainColor};
+  ${flexRowCenter};
+  width: 3.2rem;
+  height: 3.2rem;
+  border-radius: 50%;
+  background-color: ${(props) => props.theme.mainColor};
 `;
 
 const LoginUserButtonContainer = styled.div`
-    display: flex;
-    gap: 1rem;
-    position: relative;
+  display: flex;
+  gap: 1rem;
+  position: relative;
 `;
 const LoginButtonBox = styled(SearchContainer)`
-    min-width: 7.7rem;
-    height: 4.2rem;
-    font-size: 1.2rem;
-    box-shadow: 0px 3px 15px 0px rgba(0, 0, 0, 0.05);
+  min-width: 7.7rem;
+  height: 4.2rem;
+  font-size: 1.2rem;
+  box-shadow: 0px 3px 15px 0px rgba(0, 0, 0, 0.05);
 `;
 
 const MyInfoModalContainer = styled.div`
-    ${flexColumnCenter};
-    justify-content: center;
-    background-color: white;
-    width: 15rem;
-    height: 20rem;
-    ${boxBorderRadius}
-    position: absolute;
-    top: 50px;
-    right: 0px;
-    z-index: 10;
-    display: ${(props) => (props.isShow ? 'block' : 'none')};
-    box-shadow: 0px 3px 15px 0px rgba(0, 0, 0, 0.1);
-    cursor: pointer;
-    div {
-        display: flex;
-        align-items: center;
-        padding: 0px 5px;
-        border-bottom: 1px solid ${(props) => props.theme.borderColor};
-        span,
-        button {
-            font-size: 1.3rem;
-        }
+  ${flexColumnCenter};
+  justify-content: center;
+  background-color: white;
+  width: 15rem;
+  height: 20rem;
+  ${boxBorderRadius}
+  position: absolute;
+  top: 50px;
+  right: 0px;
+  z-index: 10;
+  display: ${(props) => (props.isShow ? "block" : "none")};
+  box-shadow: 0px 3px 15px 0px rgba(0, 0, 0, 0.1);
+  cursor: pointer;
+  div {
+    display: flex;
+    align-items: center;
+    padding: 0px 5px;
+    border-bottom: 1px solid ${(props) => props.theme.borderColor};
+    span,
+    button {
+      font-size: 1.3rem;
     }
+  }
 `;
 
 const UserEmail = styled.div`
-    height: 33%;
+  height: 33%;
 `;
 const UserPage = styled.div`
-    height: 33%;
-    &:hover {
-        background-color: ${(props) => props.theme.borderColor};
-    }
+  height: 33%;
+  &:hover {
+    background-color: ${(props) => props.theme.borderColor};
+  }
 `;
 const LogoutButton = styled.div`
-    height: 33%;
-    &:hover {
-        background-color: ${(props) => props.theme.borderColor};
-    }
+  height: 33%;
+  &:hover {
+    background-color: ${(props) => props.theme.borderColor};
+  }
+`;
+
+const TransparentBtn = styled.button`
+  background-color: transparent;
 `;
