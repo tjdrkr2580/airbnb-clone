595fffa863d507ae8344fbeb048a273bcb079f81
dajeong <daaa_j@naver.com>
2023-03-06 14:00:13 +0900

    feat: get wishlist

--- src/utils/api/axios.js
+++ src/utils/api/axios.js
@@ -1,33 +1,37 @@
-import axios from "axios";
+import axios from 'axios';
 
 export default class Axios {
-  constructor() {
-    this.instance = axios.create({
-      baseURL: "http://localhost:3000",
-    });
+    constructor() {
+        this.instance = axios.create({
+            baseURL: 'http://localhost:3000',
+        });
 
-    // this.instance.interceptors.request.use((request) => {
-    //   const token = getCookie("token");
-    //   if (token) {
-    //     request.headers.Authorization = `Bearer ${token}`;
-    //   }
-    // });
-  }
+        // this.instance.interceptors.request.use((request) => {
+        //   const token = getCookie("token");
+        //   if (token) {
+        //     request.headers.Authorization = `Bearer ${token}`;
+        //   }
+        // });
+    }
 
-  get = async (url) => {
-    const res = await this.instance.get(url);
-    return res;
-  };
-  post = async (url, data) => {
-    const res = await this.instance.post(url, data);
-    return res;
-  };
-  put = async (url, data) => {
-    const res = await this.instance.put(url, data);
-    return res;
-  };
-  del = async (url) => {
-    const res = await this.instance.delete(url);
-    return res;
-  };
+    get = async (url) => {
+        const res = await this.instance.get(url);
+        return res;
+    };
+    post = async (url, data) => {
+        const res = await this.instance.post(url, data);
+        return res;
+    };
+    put = async (url, data) => {
+        const res = await this.instance.put(url, data);
+        return res;
+    };
+    del = async (url) => {
+        const res = await this.instance.delete(url);
+        return res;
+    };
+    getToken = async (url, data) => {
+        const res = await this.instance.get(url, data);
+        return res;
+    };
 }
